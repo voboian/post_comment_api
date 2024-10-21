f86f112283f639b9e2ac0fb751b320eab6cd97aa
voboian <lollipopmila@gmail.com>
2024-10-21 13:42:44 +0200

    create post app and add db.sqlite3 and pycache to .gitignore

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ninja import NinjaAPI
+from posts.urls import router
+
+api = NinjaAPI()
+
+
+api.add_router("/posts/", router)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', api.urls),
 ]
